74c48ad5cd74e2a231542ee50d6e07d2240336ee
rezha4 <rezha.bahari@gmail.com>
2023-08-24 11:03:46 +0700

    solved display bug from earlier
    todo: remove button, read status button (also read data type),
    theming etc...

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let dialog = document.querySelector("dialog");
-let bookTitle = document.querySelector("input#title")
+let bookTitle = document.querySelector("input#title");
+let main = document.querySelector("main");
 
 document.querySelector("button").addEventListener("click", () => {
     dialog.showModal();
 });
 
+let library = [];
+
 document.querySelector("form button").addEventListener("click", () => {
+    removeAllChildNodes(main);
+
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
@@ -13,9 +18,17 @@ document.querySelector("form button").addEventListener("click", () => {
 
     let book = new Book(title, author, pages, read);
     addBookToLibrary(book);
+
+    displayBooks(library);
 })
 
-let library = [];
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
 
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -36,30 +49,55 @@ let quran = new Book("Al-Qur'an", "Allah", 604, "Read");
 
 addBookToLibrary(quran);
 
-let main = document.querySelector("main");
+function displayBooks(array) {
+    array.forEach((obj) => {
+        let card = document.createElement("div");
+    
+        let title = document.createElement("p");
+        title.textContent = `Title: ${obj.title}`;
+    
+        let author = document.createElement("p");
+        author.textContent = `Author: ${obj.author}`;
+    
+        let pages = document.createElement("p");
+        pages.textContent = `Pages: ${obj.pages}`;
+    
+        let readStatus = document.createElement("p");
+        readStatus.textContent = `Read status: ${obj.readStatus}`;
+    
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(readStatus);
+    
+        card.classList.add("card");
+        main.appendChild(card);
+    })
+}
 
-library.forEach((obj) => {
-    let card = document.createElement("div");
 
-    let title = document.createElement("p");
-    title.textContent = `Title: ${obj.title}`;
 
-    let author = document.createElement("p");
-    author.textContent = `Author: ${obj.author}`;
+// function displayBook (title, author, pages, read) {
+//     let card = document.createElement("div");
 
-    let pages = document.createElement("p");
-    pages.textContent = `Pages: ${obj.pages}`;
+//     let title = document.createElement("p");
+//     title.textContent = `Title: ${title}`;
 
-    let readStatus = document.createElement("p");
-    readStatus.textContent = `Read status: ${obj.readStatus}`;
+//     let author = document.createElement("p");
+//     author.textContent = `Author: ${author}`;
 
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(readStatus);
+//     let pages = document.createElement("p");
+//     pages.textContent = `Pages: ${pages}`;
 
-    card.classList.add("card");
-    main.appendChild(card);
-})
+//     let readStatus = document.createElement("p");
+//     readStatus.textContent = `Read status: ${read}`;
 
+//     card.appendChild(title);
+//     card.appendChild(author);
+//     card.appendChild(pages);
+//     card.appendChild(readStatus);
+
+//     card.classList.add("card");
+//     main.appendChild(card);
+// }
 //how to display this over and overg
